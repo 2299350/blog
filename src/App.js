0758cefa8b70ff9c3ded894b916c55dd6e8cdf2b
Alex Litsman <2299350@gmail.com>
2025-11-12 01:02:27 +0400

    Create BFF

--- src/App.js
+++ src/App.js
@@ -9,6 +9,8 @@ function App() {
 		<Div>
 			<div>123</div>
 			<i className="fa fa-camera-retro"></i> fa-camera-retro
+			<br />
+			<a href="http://localhost:4000/users">Users</a>
 		</Div>
 	);
 }
