51d75b2847772af125afc1de2f32d647a9ae2143
Alex Litsman <2299350@gmail.com>
2025-12-09 18:20:33 +0400

    Implement Styles for PostContent

--- src/bff/operations/fetch-post.js
+++ src/bff/operations/fetch-post.js
@@ -1,13 +1,6 @@
-import { ROLE } from '../constants';
 import { getPost } from '../api';
-import { sessions } from '../sessions';
 
-export const fetchPost = async (userSession, postId) => {
-	// const accessRoles = [ROLE.ADMIN];
-	// if (!sessions.access(userSession, accessRoles)) {
-	// 	return { error: 'Access is denied', res: null };
-	// }
-
+export const fetchPost = async (postId) => {
 	const post = await getPost(postId);
 
 	return {
--- src/hooks/use-server-request.js
+++ src/hooks/use-server-request.js
@@ -12,7 +12,11 @@ export const useServerRequest = () => {
 	const dispatch = useDispatch();
 
 	// список операций, которым НЕ нужна сессия
-	const noSessionOps = new Set([OPERATIONS.REGISTER, OPERATIONS.AUTHORIZE]);
+	const noSessionOps = new Set([
+		OPERATIONS.REGISTER,
+		OPERATIONS.AUTHORIZE,
+		OPERATIONS.FETCH_POST,
+	]);
 
 	// основная функция — мемоизирована
 	return useCallback(
